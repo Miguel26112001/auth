916002e39706db2a87e718eaa3e32598a4641732
Miguel26112001 <u202220294@upc.edu.pe>
2025-12-31 18:43:40 -0500

    docs: add class-level JavaDoc to `AuthenticationApplication`

--- src/main/java/com/example/authentication/AuthenticationApplication.java
+++ src/main/java/com/example/authentication/AuthenticationApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Main class for the Authentication application.
+ * Boots the Spring Boot context.
+ */
 @SpringBootApplication
 @EnableJpaAuditing
 public class AuthenticationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AuthenticationApplication.class, args);
+  }
 }
