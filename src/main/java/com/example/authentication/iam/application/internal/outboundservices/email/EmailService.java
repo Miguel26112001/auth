538185ead1859c473cd34afdc807769997782442
Miguel26112001 <u202220294@upc.edu.pe>
2025-12-31 19:03:13 -0500

    docs: add method-level Javadoc to `EmailService`

--- src/main/java/com/example/authentication/iam/application/internal/outboundservices/email/EmailService.java
+++ src/main/java/com/example/authentication/iam/application/internal/outboundservices/email/EmailService.java
@@ -1,5 +1,14 @@
 package com.example.authentication.iam.application.internal.outboundservices.email;
 
+/**
+ * Service responsible for sending emails related to user operations.
+ */
 public interface EmailService {
-    void sendVerificationEmail(String to, String verificationLink);
+  /**
+   * Sends a verification email to the given recipient.
+   *
+   * @param to email recipient
+   * @param verificationLink verification link to be sent
+   */
+  void sendVerificationEmail(String to, String verificationLink);
 }
