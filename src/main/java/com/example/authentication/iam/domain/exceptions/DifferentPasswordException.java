950e1a4700141d442b134ab35bf88876ec87d2b5
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:05:34 -0500

    feat(iam): Add javadoc to DifferentPasswordException

--- src/main/java/com/example/authentication/iam/domain/exceptions/DifferentPasswordException.java
+++ src/main/java/com/example/authentication/iam/domain/exceptions/DifferentPasswordException.java
@@ -1,6 +1,14 @@
 package com.example.authentication.iam.domain.exceptions;
 
+/**
+ * Exception thrown when the provided password does not match
+ * the current user's password.
+ */
 public class DifferentPasswordException extends RuntimeException {
+
+  /**
+   * Creates a new {@code DifferentPasswordException}.
+   */
   public DifferentPasswordException() {
     super("The password of the user is incorrect.");
   }
