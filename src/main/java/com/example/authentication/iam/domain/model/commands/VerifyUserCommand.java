c9a11da7da63f5f5c817e67164247f99f40e8640
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:30:14 -0500

    docs(iam): Add javadoc to VerifyUserCommand

--- src/main/java/com/example/authentication/iam/domain/model/commands/VerifyUserCommand.java
+++ src/main/java/com/example/authentication/iam/domain/model/commands/VerifyUserCommand.java
@@ -1,4 +1,7 @@
 package com.example.authentication.iam.domain.model.commands;
 
+/**
+ * Command used to verify a user account using a verification token.
+ */
 public record VerifyUserCommand(String token) {
 }
