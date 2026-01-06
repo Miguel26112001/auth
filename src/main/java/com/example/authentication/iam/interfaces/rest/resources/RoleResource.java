880d2f5a5d4c08e7e7645756b62ab8cca969d4cd
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:03:40 -0500

    docs(core): Add Javadoc to AuthenticatedUserResource

--- src/main/java/com/example/authentication/iam/interfaces/rest/resources/RoleResource.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/resources/RoleResource.java
@@ -1,6 +1,13 @@
 package com.example.authentication.iam.interfaces.rest.resources;
 
+/**
+ * Represents a role in the system.
+ *
+ * @param id   the ID of the role
+ * @param name the name of the role
+ */
 public record RoleResource(
     Long id,
-    String name) {
+    String name
+) {
 }
