c68c82916b26529278de92e6ccc4b81bffa9f92e
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:05:35 -0500

    feat(iam): Add Javadoc to SignUpResource
    Added Javadoc to SignUpResource record for clarity.

--- src/main/java/com/example/authentication/iam/interfaces/rest/resources/SignUpResource.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/resources/SignUpResource.java
@@ -4,9 +4,18 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
+/**
+ * Represents the request body for registering a new user.
+ *
+ * @param username the username of the new user, cannot be blank
+ * @param email the email of the new user, must be valid and not blank
+ * @param password the password of the new user, cannot be blank
+ * @param roles the list of roles to assign to the new user
+ */
 public record SignUpResource(
     @NotBlank String username,
     @NotBlank @Email String email,
     @NotBlank String password,
-    List<String> roles) {
+    List<String> roles
+) {
 }
