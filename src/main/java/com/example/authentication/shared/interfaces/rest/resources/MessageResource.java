76ab2f88df287aa563d7391f96cb62b79f513e8d
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:43:56 -0500

    docs(core): Add Javadoc to MessageResource

--- src/main/java/com/example/authentication/shared/interfaces/rest/resources/MessageResource.java
+++ src/main/java/com/example/authentication/shared/interfaces/rest/resources/MessageResource.java
@@ -1,4 +1,9 @@
 package com.example.authentication.shared.interfaces.rest.resources;
 
+/**
+ * Resource used to return a simple message in REST responses.
+ *
+ * <p>Typically used for error messages or informational responses.</p>
+ */
 public record MessageResource(String message) {
 }
