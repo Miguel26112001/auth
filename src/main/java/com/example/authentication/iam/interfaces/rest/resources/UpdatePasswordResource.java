176eb54ba7b115de84d37b6e3643bb31d6cfd700
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:05:43 -0500

    docs(core): Add Javadoc to UpdatePasswordResource

--- src/main/java/com/example/authentication/iam/interfaces/rest/resources/UpdatePasswordResource.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/resources/UpdatePasswordResource.java
@@ -2,5 +2,14 @@ package com.example.authentication.iam.interfaces.rest.resources;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record UpdatePasswordResource(@NotBlank String currentPassword, @NotBlank String newPassword) {
+/**
+ * Represents the request body for updating a user's password.
+ *
+ * @param currentPassword the user's current password, cannot be blank
+ * @param newPassword the new password to set, cannot be blank
+ */
+public record UpdatePasswordResource(
+    @NotBlank String currentPassword,
+    @NotBlank String newPassword
+) {
 }
