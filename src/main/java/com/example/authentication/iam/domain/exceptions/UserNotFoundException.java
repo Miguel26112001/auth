2b092d5e05386f2ed1716b08138e3190016774c8
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:22:04 -0500

    docs(iam): Add javadoc to UserNotFoundException

--- src/main/java/com/example/authentication/iam/domain/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/authentication/iam/domain/exceptions/UserNotFoundException.java
@@ -1,7 +1,16 @@
 package com.example.authentication.iam.domain.exceptions;
 
+/**
+ * Exception thrown when a user cannot be found.
+ */
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String username) {
-        super("User not found: " + username);
-    }
+
+  /**
+   * Creates a new {@code UserNotFoundException}.
+   *
+   * @param username username of the user that was not found
+   */
+  public UserNotFoundException(String username) {
+    super("User not found: " + username);
+  }
 }
