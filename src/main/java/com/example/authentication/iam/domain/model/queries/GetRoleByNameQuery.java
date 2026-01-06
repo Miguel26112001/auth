2985f38f706f74c1367d2081689df2f3093e2db0
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:34:41 -0500

    docs(iam): Add Javadoc for GetRoleByNameQuery

--- src/main/java/com/example/authentication/iam/domain/model/queries/GetRoleByNameQuery.java
+++ src/main/java/com/example/authentication/iam/domain/model/queries/GetRoleByNameQuery.java
@@ -1,5 +1,10 @@
 package com.example.authentication.iam.domain.model.queries;
 
+/**
+ * Query used to retrieve a role by its name.
+ *
+ * @param roles role name to be searched
+ */
 public record GetRoleByNameQuery(
     String roles) {
 }
