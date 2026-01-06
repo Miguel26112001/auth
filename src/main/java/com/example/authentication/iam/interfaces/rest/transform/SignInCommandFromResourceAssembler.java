0cacb22bd81ea55c4a4949f569380ac1cac2175b
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:11:22 -0500

    refactor(iam): Improve SignInCommand assembler
    Refactor SignInCommandFromResourceAssembler to add Javadoc and enhance readability.

--- src/main/java/com/example/authentication/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
@@ -3,11 +3,21 @@ package com.example.authentication.iam.interfaces.rest.transform;
 import com.example.authentication.iam.domain.model.commands.SignInCommand;
 import com.example.authentication.iam.interfaces.rest.resources.SignInResource;
 
+/**
+ * Assembles a {@link SignInCommand} from a {@link SignInResource}.
+ */
 public class SignInCommandFromResourceAssembler {
-  public static SignInCommand toCommandFromResource(SignInResource signInResource) {
 
+  /**
+   * Converts a {@link SignInResource} into a {@link SignInCommand}.
+   *
+   * @param signInResource the resource containing sign-in data
+   * @return the corresponding SignInCommand
+   */
+  public static SignInCommand toCommandFromResource(SignInResource signInResource) {
     return new SignInCommand(
         signInResource.username(),
-        signInResource.password());
+        signInResource.password()
+    );
   }
 }
