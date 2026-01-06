1da78989ff975fee64f95e154a43d4878f0e863a
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:20:59 -0500

    docs(iam): Add javadoc to UserNotActiveException

--- src/main/java/com/example/authentication/iam/domain/exceptions/UserNotActiveException.java
+++ src/main/java/com/example/authentication/iam/domain/exceptions/UserNotActiveException.java
@@ -1,6 +1,15 @@
 package com.example.authentication.iam.domain.exceptions;
 
+/**
+ * Exception thrown when a user account is not active.
+ */
 public class UserNotActiveException extends RuntimeException {
+
+  /**
+   * Creates a new {@code UserNotActiveException}.
+   *
+   * @param username username of the inactive user
+   */
   public UserNotActiveException(String username) {
     super("User is not active: " + username);
   }
