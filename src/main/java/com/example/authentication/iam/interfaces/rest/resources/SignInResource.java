703a1804efbe128ca03405589e06c7e3053b6b7f
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:03:46 -0500

    docs(iam): Add Javadoc to SignInResource

--- src/main/java/com/example/authentication/iam/interfaces/rest/resources/SignInResource.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/resources/SignInResource.java
@@ -1,6 +1,13 @@
 package com.example.authentication.iam.interfaces.rest.resources;
 
+/**
+ * Represents the request body for signing in a user.
+ *
+ * @param username the username of the user
+ * @param password the password of the user
+ */
 public record SignInResource(
     String username,
-    String password) {
+    String password
+) {
 }
