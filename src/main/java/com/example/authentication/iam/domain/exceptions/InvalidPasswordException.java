3b8199712bbacfaef15a49e6de1644e23a113aa3
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:09:08 -0500

    docs(iam): Add javadoc to InvalidPasswordException

--- src/main/java/com/example/authentication/iam/domain/exceptions/InvalidPasswordException.java
+++ src/main/java/com/example/authentication/iam/domain/exceptions/InvalidPasswordException.java
@@ -1,7 +1,14 @@
 package com.example.authentication.iam.domain.exceptions;
 
+/**
+ * Exception thrown when a provided password is invalid.
+ */
 public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException() {
-        super("Invalid password");
-    }
+
+  /**
+   * Creates a new {@code InvalidPasswordException}.
+   */
+  public InvalidPasswordException() {
+    super("Invalid password");
+  }
 }
