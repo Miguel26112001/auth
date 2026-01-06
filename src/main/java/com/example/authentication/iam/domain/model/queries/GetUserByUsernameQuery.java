c4094c4f10b50884e11558fbcf3e9907b30f42f0
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-05 20:36:24 -0500

    docs(iam): Add Javadoc to GetUserByUsernameQuery

--- src/main/java/com/example/authentication/iam/domain/model/queries/GetUserByUsernameQuery.java
+++ src/main/java/com/example/authentication/iam/domain/model/queries/GetUserByUsernameQuery.java
@@ -1,5 +1,10 @@
 package com.example.authentication.iam.domain.model.queries;
 
+/**
+ * Query used to retrieve a user by its username.
+ *
+ * @param username unique username of the user
+ */
 public record GetUserByUsernameQuery(
     String username) {
 }
