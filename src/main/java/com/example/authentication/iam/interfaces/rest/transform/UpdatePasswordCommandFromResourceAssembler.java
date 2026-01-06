e210c29d695ee7b5ae7e935fb7ee009ae83faf50
Miguel26112001 <u202220294@upc.edu.pe>
2026-01-06 10:13:28 -0500

    refactor(iam): Improve UpdatePasswordAssembler documentation

--- src/main/java/com/example/authentication/iam/interfaces/rest/transform/UpdatePasswordCommandFromResourceAssembler.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/transform/UpdatePasswordCommandFromResourceAssembler.java
@@ -3,8 +3,26 @@ package com.example.authentication.iam.interfaces.rest.transform;
 import com.example.authentication.iam.domain.model.commands.UpdatePasswordCommand;
 import com.example.authentication.iam.interfaces.rest.resources.UpdatePasswordResource;
 
+/**
+ * Assembles a {@link UpdatePasswordCommand} from a {@link UpdatePasswordResource}.
+ */
 public class UpdatePasswordCommandFromResourceAssembler {
-    public static UpdatePasswordCommand toCommandFromResource(Long userId, UpdatePasswordResource resource) {
-        return new UpdatePasswordCommand(userId, resource.currentPassword(), resource.newPassword());
-    }
+
+  /**
+   * Converts an {@link UpdatePasswordResource} into an {@link UpdatePasswordCommand}.
+   *
+   * @param userId  the ID of the user whose password is being updated
+   * @param resource the resource containing current and new password
+   * @return the corresponding UpdatePasswordCommand
+   */
+  public static UpdatePasswordCommand toCommandFromResource(
+      Long userId,
+      UpdatePasswordResource resource) {
+
+    return new UpdatePasswordCommand(
+        userId,
+        resource.currentPassword(),
+        resource.newPassword()
+    );
+  }
 }
